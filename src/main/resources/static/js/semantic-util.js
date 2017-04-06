3800f00e71cca05273a8d450d4799df38b0fd545
ZhongWen Li <lao-tse@qq.com>
2017-04-06 23:12:09 +0800

    Update

---
+++ src/main/resources/static/js/semantic-util.js
@@ -0,0 +1,16 @@
+
+var laotse = {};
+
+laotse.isEmpty = function(str) {
+  if (null == str || "string" != typeof(str)) {
+    return true;
+  }
+  return str.trim().length == 0;
+}
+
+laotse.isNotEmpty = function(str) {
+  if (null == str || "string" != typeof(str)) {
+    return false;
+  }
+  return str.trim().length > 0;
+}
