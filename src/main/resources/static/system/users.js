52840f959fc3ee9264889cfe64df2dbf101c7ad3
ZhongWen Li <lao-tse@qq.com>
2017-04-06 23:28:59 +0800

    Update

---
+++ src/main/resources/static/system/users.js
@@ -0,0 +1,66 @@
+
+$(function () {
+  loadUsers();
+});
+
+function loadUsers() {
+  $.ajax({
+    url: "/api/v1/users",
+    method: "get",
+    contentType: "application/json; charset=UTF-8",
+    success: function (result) {
+      console.log(result);
+      if (result) {
+        fillTableBody(result);
+      }
+    },
+    error: function (result) {
+      console.log(result);
+    }
+  });
+}
+
+function fillTableBody(users) {
+  if (null == users || 0 == users.length) {
+    return;
+  }
+  var html = "";
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    console.log(user);
+    html += ""
+      + "<tr>"
+      + "  <td>" + (user.id || "-") + "</td>"
+      + "  <td>" + (user.name || "-") + "</td>"
+      + "  <td>" + (user.email || "-") + "</td>"
+      + "  <td>" + (getGenderDictName(user.gender) || "-") + "</td>"
+      + "  <td>"
+      + "    <a class=\"delete link\" uid=\"" + user.id + "\"><i class=\"delete icon red\"></i></a>"
+      + "    <span style=\"padding-left:10px;\">&nbsp;</span>"
+      + "    <a class=\"edit link\" uid=\"" + user.id + "\"><i class=\"edit icon\"></i></a>"
+      + "  </td>"
+      + "</tr>";
+  }
+  $("#js-tbody-users").empty().append(html);
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    $(".delete.link").off("click").on("click", function () {
+      remove($(this).attr("uid"));
+    });
+    $(".edit.link").off("click").on("click", function () {
+      edit($(this).attr("uid"));
+    });
+  }
+}
+
+function getGenderDictName(id) {
+  return "M" == id ? "男" : "女";
+}
+
+function remove(uid) {
+  console.log(uid);
+}
+
+function edit(uid) {
+  console.log(uid);
+}
